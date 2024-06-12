547316df3d200b59e039ff247d17279ad6e8cfce
Jesus Alberto Carballo Olachea <jesusalbec_21@alu.uabcs.mx>
2024-06-11 17:56:47 -0700

    development 90%

--- routes/ReportesMaltrato.js
+++ routes/ReportesMaltrato.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const connection = require('../config/db');
 var router = express.Router();
-
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
@@ -17,9 +17,27 @@ router.get('/', async function (req, res, next) {
 
 });
 
-router.post('/', async function (req, res, next) {
+function obtenerUsuario(req, res, next) {
+    if (req.body.token) {
+        jwt.verify(req.body.token, "asd123", (error, data) => {
+            if (error) {
+                console.log(error);
+                res.sendStatus(403);
+            } else {
+                req.body.idUser = data.id_usuario;
+                req.body.username = data.nombre + " " + data.primer_apellido + " " + data.segundo_apellido;
+            }
+        });
+        next();
+    } else {
+        res.sendStatus(403);
+    };
+};
+
+router.post('/', obtenerUsuario, async function (req, res, next) {
     try {
-        const { idUser, username, petType, breed, description, gender, doa } = req.body;
+        const { idUser, username } = req.body;
+        const { petType, breed, description, gender, doa } = req.body.form;
         //          { "idUser": "1",
         // "username": "Roberto", 
         // "petType": "perro", 
@@ -28,11 +46,13 @@ router.post('/', async function (req, res, next) {
         // "gender": "macho", 
         // "doa": "2024-05-30"
         // }
+        console.log(req.body);
         if (!idUser || !username || !petType || !breed || !description || !gender || !doa) {
             return res.status(400).json({ status: 400, message: "Invalid request data" });
         }
-        const [result] = await connection.query('insert into reporteMaltrato (id_usuario,nombre_usuario,tipo_mascota,raza,descripcion,genero,fecha_maltrato) values (?, ?, ?, ?, ?, ?, ?)', [idUser, username, petType, breed, description, gender, doa ]);
-        res.status(200).json({ status: 200, message: "Item created successfully", data: { id: result.insertId, idUser, username, petType, breed, description, gender, doa  } });
+        console.log("holi");
+        const [result] = await connection.query('insert into reporteMaltrato (id_usuario,nombre_usuario,tipo_mascota,raza,descripcion,genero,fecha_maltrato) values (?, ?, ?, ?, ?, ?, ?)', [idUser, username, petType, breed, description, gender, doa]);
+        res.status(200).json({ status: 200, message: "Item created successfully", data: { id: result.insertId, idUser, username, petType, breed, description, gender, doa } });
     } catch (err) {
         res.status(500).json({ status: 500, message: err.message });
     }
